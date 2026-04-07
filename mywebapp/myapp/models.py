cad7e98183c346fe94405e0c7090657d883af2e7
Namrah Arfin <namraharfin30@gmail.com>
2026-04-07 09:41:16 +0530

    Integrated frontend with backend model and displayed data in table

--- mywebapp/myapp/models.py
+++ mywebapp/myapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class MyModel(models.Model):
+    a = models.IntegerField()
+    b = models.CharField(max_length=255)
+    c = models.CharField(max_length=100)
+    d = models.FloatField()
+    
+
+class MyTable(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255 , unique=True)
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- mywebapp/myapp/views.py
+++ mywebapp/myapp/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.template import loader
+from .models import Member, MyTable
+from django.db import IntegrityError
+from django.contrib import messages
+
+def home(request):
+    return HttpResponse("Hello, World!")
+
+def insert_data(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        subject = request.POST.get("subject")
+        email = request.POST.get("email")
+
+        try:
+            MyTable.objects.create(name=name, subject=subject, email=email)
+            messages.success(request, "Data inserted successfully")
+        except IntegrityError:
+            messages.error(request, "Email already exists")
+            return render(request, "form.html", {"error": "Email already exists"})
+        return redirect("insert")
+
+    return render(request, "form.html")
+
+def form_view(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        subject = request.POST.get('subject')
+        email = request.POST.get('email')
+
+        MyTable.objects.create(
+            name=name,
+            subject=subject,
+            email=email
+        )
+
+    return render(request, 'form.html')
+
+def show_data(request):
+    data = MyTable.objects.all()
+    return render(request, 'show.html', {'data': data})
